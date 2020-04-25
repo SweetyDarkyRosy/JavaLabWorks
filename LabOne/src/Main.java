f74e3dc09f94dd509339b8190182d65c58731897
Viktoriia <sweetydarkyrosy@gmail.com>
2020-04-25 14:47:40 +0300

    Laboratory works #1 Main.java has been refactored

--- LabOne/src/Main.java
+++ LabOne/src/Main.java
@@ -9,83 +9,72 @@ import java.util.Arrays;
 
 public class MyApp
 {
-    public static boolean strToInt(String str)
-    {
-        try
-        {
-            Integer.parseInt(str);
-            return true;
-        }
-        catch (NumberFormatException e)
-        {
-            return false;
-        }
-    }
+	public static boolean isPrime(int i)
+	{
+		if (i <= 1)
+		{
+			return false;
+		}
+		else if (i <= 3)
+		{
+			return true;
+		}
+		else if (i % 2 == 0 || i % 3 == 0)
+		{
+			return false;
+		}
 
-    public static boolean isPrime(int i)
-    {
-        if (i <= 1)
-        {
-            return false;
-        }
-        else if (i <= 3)
-        {
-            return true;
-        }
-        else if (i % 2 == 0 || i % 3 == 0)
-        {
-            return false;
-        }
+		int n = 5;
+		while (n * n <= i)
+		{
+			if (i % n == 0 || i % (n + 2) == 0)
+			{
+				return false;
+			}
+			n += 6;
+		}
+		return true;
+	}
 
-        int n = 5;
-        while (n * n <= i)
-        {
-            if (i % n == 0 || i % (n + 2) == 0)
-            {
-                return false;
-            }
-            n += 6;
-        }
-        return true;
-    }
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(System.in);
+		int min = 0, max = 0;
+		String str;
 
-    public static void main(String[] args)
-    {
-        Scanner scan = new Scanner(System.in);
-        int min = 0, max = 0;
-        String str;
+		System.out.println("Enter min: ");
+		while (!scan.hasNextInt())
+		{
+			scan.next();
+			System.out.println("Enter integer number! Enter min: ");
+		}
+		min = scan.nextInt();
 
-        System.out.println("Enter min: ");
-        while (!strToInt(str = scan.next()))
-        {
-            System.out.println("Enter integer number! Enter min: ");
-        }
-        min = Integer.parseInt(str);
+		System.out.println("Enter max: ");
+		while (!scan.hasNextInt())
+		{
+			scan.next();
+			System.out.println("Enter integer number! Enter min: ");
+		}
+		max = scan.nextInt();
+		scan.close();
 
-        System.out.println("Enter max: ");
-        while (!strToInt(str = scan.next()))
-        {
-            System.out.println("Enter integer number! Enter max: ");
-        }
-        max = Integer.parseInt(str);
-        scan.close();
+		System.out.println("Min: " + min);
+		System.out.println("Max: " + max);
 
-        System.out.println("Min: " + min);
-        System.out.println("Max: " + max);
+		if (max < min)
+		{
+			int tempInt = max;
+			max = min;
+			min = tempInt;
+		}
 
-        if (max < min)
-        {
-            int tempInt = max;
-            max = min;
-            min = tempInt;
-        }
-
-        for (int i = min; i <= max; i++)
-        {
-            if (isPrime(i))
-            {
-                System.out.println(i + " is a prime number");
-            }
-        }
-    }
+		for (int i = min; i <= max; i++)
+		{
+			if (isPrime(i))
+			{
+				System.out.println(i + " is a prime number");
+			}
+		}
+	}
 }
